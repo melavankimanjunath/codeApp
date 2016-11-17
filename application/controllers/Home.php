cd17b5a53e6ac1efe40ce5571d44cb85a195896e
Manjunath Melavanki <mmelav@sapient.com>
2016-11-17 17:12:17 +0530

    veiw dispay fuctiion is created

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -10,8 +10,17 @@ Class Home extends CI_Controller
             echo "File does not exist";
         }
         $data['title'] = ucfirst("Home");
+        $this->show($data);
+    }
+    public function index()
+    {
+        $data['title'] = ucfirst("Index");
+        $this->show($data);
+    }
+    public function show($data)
+    {
         $this->load->view('templates/header', $data);
-        $this->load->view('home/'.$page);
+        $this->load->view('home/home');
         $this->load->view('templates/footer', $data);
     }
 }
